76537523f0bc2f7d587d744c63a9398382494ca4
yafimm <9e.yafi.maulana@gmail.com>
2019-05-27 03:05:12 +0700

    Add Routing in React

---
+++ src/appRouter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import App from "./components/home/App";
+import Contact from "./components/page/Contact";
+
+const AppRouter = () => {
+  return(
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={App} />
+        <Route path="/contact" exact component={Contact} />
+      </div>
+    </Router>
+  );
+};
+
+export default AppRouter;
---
+++ src/components/page/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return(
+    <div>
+      <h1>Halaman Contact</h1>
+      <p>Hub Kami : 083822200488</p>
+    </div>
+  );
+}
+
+export default Contact;
